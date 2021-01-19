3b65e29253011f910bbfaed2f8480518cb51298e
ShibilPM <shibilpm3232@gmail.com>
2021-01-19 19:33:31 +0530

    Add Movie Details

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 //Router
 import { Switch, Route } from "react-router-dom";
+import Moviedetail from "./pages/MovieDetail";
 
 function App() {
   return (
@@ -18,8 +20,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <Moviedetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
